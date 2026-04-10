f56c1d403cc4d2fe0dffc621913c3306678ded93
Maycon Rodrigo Moreira <maycon.moreira@intelbras.com.br>
2026-04-10 15:47:08 -0300

    chore(option): Add option args

--- app/main.c
+++ app/main.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include "librouteros.h"
 #include "parse_items.h"
+#include "options.h"
 
 //Possível usar curl
 //curl -k -u admin:admin http://192.168.0.254/rest/ip/firewall/nat
 
-#define DEBUG 0
 
 void send_command(struct ros_connection *conn, char *command, No** lista_resultados) {
     
@@ -33,9 +33,9 @@ void send_command(struct ros_connection *conn, char *command, No** lista_resulta
                 }
             }
             
-            // Aloca espaço para os retornos
-            dado_atual.total = num_itens;
-            dado_atual.retorno = (char**)malloc(num_itens * sizeof(char*));
+            // Aloca espaço para os retornos (+1 para o ID)
+            dado_atual.total = num_itens + 1;
+            dado_atual.retorno = (char**)malloc(dado_atual.total * sizeof(char*));
             
             if (!dado_atual.retorno) {
                 printf("Erro ao alocar memória\n");
@@ -43,8 +43,18 @@ void send_command(struct ros_connection *conn, char *command, No** lista_resulta
                 break;
             }
             
-            // Preenche os dados
-            int idx = 0;
+            // Adiciona o campo ID no início
+            char id_buffer[32];
+            snprintf(id_buffer, sizeof(id_buffer), "=ID=%d", entrada);
+            dado_atual.retorno[0] = (char*)malloc((strlen(id_buffer) + 1) * sizeof(char));
+            if (dado_atual.retorno[0]) {
+                strcpy(dado_atual.retorno[0], id_buffer);
+                if(DEBUG)
+                    printf("\033[30;42;6m[DADOS ANTES DE INSERIR NA LISTA]:\033[m{%s}\n", id_buffer);
+            }
+            
+            // Preenche os dados originais a partir do índice 1
+            int idx = 1;
             for (int i = 1; i < res->sentence->words; i++) {
                 if (res->sentence->word[i][0] == '=') {
                     // Aloca e copia a string (pula o '=' inicial)
@@ -70,7 +80,8 @@ void send_command(struct ros_connection *conn, char *command, No** lista_resulta
             free(dado_atual.retorno);
         }
         else if (res->done) {
-            printf("Total de entradas processadas: %d\n", entrada);
+            if(DEBUG)
+                printf("Total de entradas processadas: %d\n", entrada);
             ros_result_free(res);
             break;
         }
@@ -82,27 +93,39 @@ void send_command(struct ros_connection *conn, char *command, No** lista_resulta
         ros_result_free(res);
     }
 }
-int main() {
+int main(int argc, char** argv) {
+
+
+        Config config;
+    
+    // Parse das opções
+    parse_options(argc, argv, &config);
+    
+    // Verifica se pediu ajuda
+    if (config.help) {
+        print_help(argv[0]);
+        free_config(&config);
+        return 0;
+    }
+
+    int port = 0;
+
     struct ros_connection *conn;
     
-
-    char *host = "192.168.0.254";
-    char *user = "admin";
-    char *pass = "admin";
-
-    conn = ros_connect(host, 8728);
+    port = atoi( config.port);
+    conn = ros_connect(config.host,port);
     if (!conn) {
         printf("Erro ao conectar\n");
         return 1;
     }
     
-    if (!ros_login(conn, user, pass)) {
+    if (!ros_login(conn,config.user,config.password)) {
         printf("Erro no login\n");
         ros_disconnect(conn);
         return 1;
     }
-
-    printf("\033[30;42;1mConectado com sucesso ao MikroTik!\033[m\n");
+    if(DEBUG)
+        printf("\033[30;42;1mConectado com sucesso ao MikroTik!\033[m\n");
 
 
 
@@ -123,12 +146,15 @@ int main() {
     // Libera a memória no final
     //liberar_lista(&lista);
 
-    printf("\n=== IMPRIMINDO TODOS OS RESULTADOS FILTER ===\n");
-    send_command(conn, "/ip/firewall/nat/print", &lista);
+    if(DEBUG)
+        printf("\n=== IMPRIMINDO TODOS OS RESULTADOS FILTER ===\n");
+    send_command(conn, config.commands, &lista);
 
     imprimir_lista(lista);
-    printf("\n=== PRIMEIRO RESULTADO FILTER ===\n");
-    imprimir_dado_especifico(lista, 0);
+    if(DEBUG){
+        printf("\n=== PRIMEIRO RESULTADO FILTER === lista->info.total[%d]\n", lista->info.total);
+        imprimir_dado_especifico(lista, 0, 1);
+    }
 
     liberar_lista(&lista);
 
